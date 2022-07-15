4ab0a54adf73e518b58a542bf21ab448d1c4ad22
Lenin <leninalbino@gmail.com>
2022-07-14 20:14:33 -0500

    dsdsd

---
+++ src/main/java/com/idat/MicroservicioGrupo/MicroservicioGrupoApplication.java
@@ -0,0 +1,13 @@
+package com.idat.MicroservicioGrupo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioGrupoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioGrupoApplication.class, args);
+	}
+
+}
